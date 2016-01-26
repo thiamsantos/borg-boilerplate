761661bf9d192776d2fcae9f1f1acf6de6e07e53
thiamdossantos <thia.mdossantos@gmail.com>
2016-01-26 15:56:41 -0200

    webserver added

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,49 @@
 var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
 var sass =  require('gulp-sass');
 var uglify = require('gulp-uglify');
 var watch = require('gulp-watch');
 var include = require('gulp-include');
-var pngquant = require('imagemin-pngquant');
+var gutil = require('gulp-util');
+var jshint = require('gulp-jshint');
+var sourcemaps = require('gulp-sourcemaps');
+var connect = require('gulp-connect');
 
-var js = "./src/js/main.js";
-var css = "./src/sass/*.scss";
-var components = "./src/sass/**";
-var img = "./src/img/**";
+var mainjs = './src/js/main.js';
+var js = './src/js/**';
+var css = './src/sass/*.scss';
+var components = './src/sass/**';
+var img = './src/img/**';
+
+// Server task
+gulp.task('server', function () {
+  connect.server({
+    root: ['.'],
+    port: 3000,
+    livereload: true
+  });
+});
 
 // Javascript task
 gulp.task('js-task', function() {
-  gulp.src(js)
+  gulp.src(mainjs)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(sourcemaps.init())
     .pipe(include())
-    .pipe(uglify())
+    .pipe(uglify().on('error', gutil.log))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./dist/js'));
 });
 
-// Sass task
+// Sass Task
 gulp.task('sass-task', function() {
   gulp.src(css)
+    .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest('./dist/css'));
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/css/'));
 });
 
 // Images tasks
@@ -33,23 +53,20 @@ gulp.task('img-task', function() {
       progressive: true,
       use: [pngquant()]
     }))
-    .pipe(gulp.dest('./dist/img'))
+    .pipe(gulp.dest('./dist/img'));
 });
 
 // Default task
 gulp.task('default', function() {
-  gulp.run('js-task', 'sass-task', 'img-task');
+  gulp.run('js-task', 'sass-task', 'img-task', 'server');
 
   watch(js, function(){
-    gulp.run('js-task');
-  });
-  watch(css, function(){
-    gulp.run('sass-task');
+    gulp.run('js-task').pipe(connect.reload());
   });
   watch(components, function(){
-    gulp.run('sass-task');
+    gulp.run('sass-task').pipe(connect.reload());
   });
   watch(img, function(){
-    gulp.run('img-task');
+    gulp.run('img-task').pipe(connect.reload());
   });
 });
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,3 @@
 //=require components/jquery-1.11.3.min.js
 //=require components/materialize.js
 
-console.log("Olá Mundo");
